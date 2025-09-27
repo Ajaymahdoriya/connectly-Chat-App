d7a7757273f373784b7d79500e5e2df9e691df77
Ajaymahdoriya <ajay.mahdoriya7505@gmail.com>
2025-09-28 00:58:24 +0530

    chore: add utility functions and conversation management logic for user interactions

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://relevant-skylark-2.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: 'https://relevant-skylark-2.clerk.accounts.dev',
+        applicationID: 'convex',
+      },
+    ],
+  };
